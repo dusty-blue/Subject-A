0c155599b9056b6fe0f39972ed69beec765735bf
Helen Arnold <h.arnold@campus.tu-berlin.de>
2019-05-25 13:06:05 +0200

    reactions are now used in order they were given

--- index.js
+++ index.js
@@ -9,18 +9,17 @@ client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
-client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- else if(msg.content.startsWith('!poll')){
+client.on('message', async msg => {
+    if (msg.content.startsWith('!poll')) {
 	 console.log('saw: ' + msg.content);
 	 var reactions= msg.content.match(/\d+(?=\>)|(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g);
 	 console.log(reactions);
 	 for (var i=0; i< reactions.length; i++){
-		 msg.react(reactions[i])
-			.then(console.log)
-			.catch(console.error); //e? e.id :"305818615712579584"  client.emojis.randomKey()
+	     try {
+             await msg.react(reactions[i])
+	     } catch(e){
+             console.error('Failed to react \n'+ e)
+	     }
 	 }
  }
 });
