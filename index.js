6cbb5ab3aa7207943940a7ec022f706d4cd621d1
Helen Arnold <h.arnold@campus.tu-berlin.de>
2019-09-19 23:52:25 +0200

    removed attachements and added the question to the result message

--- index.js
+++ index.js
@@ -54,7 +54,9 @@ client.on('message', async msg => {
             try {
                 const buffer = await graph.toBuffer('image/png');
                 const attachment = new Attachment(buffer, 'awesome.png');
-                msg.channel.send(printResults(counts), attachment); 
+                const question = msg.content.match(/poll[\s\w+]+\?/g); 
+                console.log(question);
+                msg.channel.send(question + '\n' + printResults(counts)); 
             } catch(e) {
                 console.error('Failed to send results with error:\n' + e);
             }
